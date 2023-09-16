9f7352c06fd576fe470cf6bd05e8fc4250f6a957
KoalaQuakes <flame.frost@gmail.com>
2023-09-17 01:45:12 +0200

    Inheritance
    direct parent - child relation

--- Testing/scope.py
+++ Testing/scope.py
@@ -14,6 +14,13 @@ class Monster:
     def get_damage(self,amount):
         self.health -= amount
 
+    def attack(self, amount):
+        print(f'The Monster has attacked and dealt {amount} damage')
+        self.energy -= 20
+
+    def move(self,speed):
+        print(f'The monster has moved with a speed of {speed} speed')
+
 
 monster = Monster(health = 100, energy = 50, mana = 50)
 
@@ -38,3 +45,32 @@ print(monster.health)
 
 hero.attack()
 print(monster.health)
+
+class Shark(Monster):
+    def __init__(self, speed, health, energy, mana):
+        super().__init__(health, energy, mana)
+        #Monster.__init__(self, health, energy, mana)
+        self.speed = speed
+
+    def bite(self):
+        print('the shark has bitten')
+
+    def move(self):
+        print('the shark has moved')
+        print(f'the speed of the shark is {self.speed}')
+
+shark = Shark(speed = 50, health = 90, energy = 75, mana = 45)
+print(shark.mana, shark.bite())
+
+class Scorpion(Monster):
+    def __init__(self, poison, scorpion_health, scorpion_energy, scorpion_mana):
+        super().__init__(scorpion_health, scorpion_energy, scorpion_mana)
+        self.posion = poison
+
+    def attack(self):
+        print('The scorpion has attacked')
+        print(f'it has dealt {self.posion} poison damage')
+
+scorpion = Scorpion(poison = 50, scorpion_health = 20, scorpion_energy = 75, scorpion_mana = 50)
+scorpion.attack()
+scorpion.move(5)
